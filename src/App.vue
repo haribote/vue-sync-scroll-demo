63edbe0c60b49149ead0101ffa592757a9e8c9b9
KIMURA Tetsuro <haribote.nobody@gmail.com>
2017-02-06 14:33:26 +0900

    define SectionItem component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <section-item v-for="item in itemsList" :name="item.name"></section-item>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello';
+import SectionItem from './components/SectionItem';
 
 export default {
   name: 'app',
   components: {
-    Hello,
+    SectionItem,
+  },
+  data() {
+    return {
+      itemsList: [
+        {
+          name: 'raw0027',
+        },
+      ],
+    };
   },
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: sans-serif;
 }
 </style>
